eb96c545702f588c0149c28040dcdd14313d8915
Lars Kuhtz <lars@kadena.io>
2025-03-11 11:54:38 -0700

    Require QuickCheck >=2.12

--- test/Main.hs
+++ test/Main.hs
@@ -50,23 +50,6 @@ import Test.QuickCheck
 import Data.MerkleLog
 
 -- -------------------------------------------------------------------------- --
--- Support for QuickCheck < 2.12
-
-#if ! MIN_VERSION_QuickCheck(2,12,0)
-infix 4 =/=
-(=/=) :: (Eq a, Show a) => a -> a -> Property
-x =/= y = counterexample (show x ++ interpret res ++ show y) res
-  where
-    res = x /= y
-    interpret True  = " /= "
-    interpret False = " == "
-
-isSuccess :: Result -> Bool
-isSuccess Success{} = True
-isSuccess _ = False
-#endif
-
--- -------------------------------------------------------------------------- --
 -- Main
 
 main :: IO ()
